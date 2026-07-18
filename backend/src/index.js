90a7a366464b1104a9c0a4329ac6c0e49fd90c53
FaizanAhmed2008 <forfaizanahmed@gmail.com>
2026-07-18 21:44:30 +0530

    .... Nothing Change

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('SERVER IS RUNNING!');
 });
 
 const startServer = async () => {
